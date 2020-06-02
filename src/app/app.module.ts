ca2d56f7bbba44b7915443c04f08aa5315d15685
Sathish Jayapal <sjayapal@skminfotech.com>
2020-06-02 18:30:29 -0500

    Commits to get project into github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonaldetComponentComponent } from './personaldet-component/personaldet-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaldetComponentComponent
   ],
   imports: [
     BrowserModule,
